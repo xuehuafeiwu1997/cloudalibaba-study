09f9e9e90d5f697b40814b08496d6a6588b7429a
xuehuafeiwu1997 <2500207996@qq.com>
2024-04-01 16:15:44 +0800

    nacos接入配置中心，并且能够动态获取到对应的值

---
+++ nacos-provider/src/main/java/com/xmy/nacosprovider/controller/ConfigController.java
@@ -0,0 +1,42 @@
+package com.xmy.nacosprovider.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+
+/**
+ * @author xmy
+ * @date 2024/3/23 17:17
+ */
+@Controller
+@RequestMapping("config")
+@RefreshScope
+public class ConfigController {
+
+    @NacosValue(value = "${useLocalCache:false}", autoRefreshed = true)
+    private boolean useLocalCache;
+
+    @Value("${nickName}")
+//    @NacosValue(value = "${nickName}", autoRefreshed = true)
+    private String nickName;
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @Value("${spring.cloud.nacos.config.fileExtension}")
+    private String fileExtension;
+
+
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
+    @ResponseBody
+    public String get() {
+        System.out.println("application:" + applicationName);
+        System.out.println("fileExtension:" + fileExtension);
+        return nickName;
+    }
+}
